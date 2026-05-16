4c7f2c695f0e64bed7257f8b57d24f559c792d10
Joel Herrera Vázquez <joel@astro.unam.mx>
2026-05-16 11:23:24 -0700

    Use pyproject metadata as package source of truth

--- setup.py
+++ setup.py
@@ -1,18 +1,4 @@
 from setuptools import setup
 
-setup(
-    name='KrakenOS',
-    version='1.0.0.24',
-    packages=['KrakenOS', 'KrakenOS.AstroAtmosphere', 'KrakenOS.Cat', 'KrakenOS.Examples', 'KrakenOS.Docs', 'KrakenOS.LensCat'],
 
-    install_requires=['pyvista','PyVTK','vtk','numpy','scipy','matplotlib', 'csv342', 'pandas' ],
-    package_data={'': ['LICENSE.txt', '../*.AGF', '../*.agf', '../*.stl', '../*.scad', '../*.pdf', '../*.zmx', '../*.ZMX', '../*.csv']},
-    include_package_data=True,
-    url='https://github.com/Garchupiter/Kraken-Optical-Simulator',
-    download_url='https://github.com/Garchupiter/Kraken-Optical-Simulator',
-    keywords=['Optical', 'Simulator', 'lens'],
-    license='GNU General Public License v3.0',
-    author='joel Herrera et al.',
-    author_email='joel@astro.unam.mx',
-    description='Optical Simulation and ray tracing '
-)
+setup()
